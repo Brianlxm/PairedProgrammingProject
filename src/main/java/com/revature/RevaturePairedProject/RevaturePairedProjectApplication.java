880fcfd87f4011794164ecfcbe9238c77669a45f
eunhanlee <eunhanlee58@gmail.com>
2024-04-12 16:22:18 -0400

    Initial commit

---
+++ src/main/java/com/revature/RevaturePairedProject/RevaturePairedProjectApplication.java
@@ -0,0 +1,13 @@
+package com.revature.RevaturePairedProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevaturePairedProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevaturePairedProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/RevaturePairedProject/RevaturePairedProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.RevaturePairedProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevaturePairedProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
